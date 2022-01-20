a089cba797ff2a96835f2d7655dff6c5ff319a7f
Divyajg <guttuladivyajyothi@gmail.com>
2022-01-20 12:46:10 +0100

    Deploy

---
+++ src/data.js
@@ -0,0 +1,48 @@
+import mealshare from "./images/mealShare.jpg";
+import weatherApp from "./images/weatherApp.JPG";
+import gifSearch from "./images/gifSearch.JPG";
+import todoList from "./images/todoList.JPG";
+import userSearch from "./images/userSearch.JPG";
+
+export const projects = [{
+        title: "Meal Sharing",
+        subtitle: "React.js, Node.js, Knex, MySQL, CSS and HTML",
+        description: "An exclusive App, where you can reserve to share a meal with others, Share your interesting meal and Review the meals.",
+        image: mealshare,
+        link: "https://hyf-meal-sharing-dj.herokuapp.com/",
+    },
+    {
+        title: "Weather App",
+        subtitle: "HTML, CSS and JavaScript(Fetch API and DOM Manipulation)",
+        description: "Applications that provides you the weather details of you current location or any other location",
+        image: weatherApp,
+        link: "https://divyajg.github.io/weather-Application/weather.html",
+    },
+    {
+        title: "GitHub User Search",
+        subtitle: "React.js(Hooks, Context, Lifecycle methods)",
+        description: "You can search for the GitHub profiles by name",
+        image: userSearch,
+        link: "https://github-user-search-dj.herokuapp.com/",
+    },
+    {
+        title: "Giphy Search",
+        subtitle: "HTML, CSS and JavaScript(Fetch API and DOM Manipulation)",
+        description: "You can search the gifs here and limit the number of results you want",
+        image: gifSearch,
+        link: "https://divyajg.github.io/Giphy-Search/aGifSearch.html",
+    },
+    {
+        title: "Todolist",
+        subtitle: "React.js(Hooks, JSON data, Lifecycle methods)",
+        description: "Todolist application provides to add the todo to your list, edit & update it, mark it done and to delete that todo.",
+        image: todoList,
+        link: "https://todolist-app-dj.herokuapp.com/",
+    },
+];
+
+export const hardSkills = ["HTML(5)", "CSS(3)", "JavaScript(+ES6)", "React.js", "Node.js", "Knex", "MySql", "Git", "GitHub", "Rest API's", "Express.js",
+    "Docker"
+];
+
+export const softSkills = ["Adaptable", "Continous-learning", "Approachable", "Creative", "MultiCultural", "Problem-Solving"];
